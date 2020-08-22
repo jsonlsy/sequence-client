621c5677bb149d63d82cb993fa58224e7d772b2e
Jason Lee Sui Yuen <jsonlsy@gmail.com>
2020-08-22 17:33:26 +1000

    Setup client and socket io

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 8080,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            // {
+            //     test: /\.scss$/,
+            //     use: [{
+            //         loader: 'style-loader'
+            //     }, {
+            //         loader: 'css-loader',
+            //     }, {
+            //         loader: 'sass-loader',
+            //     }]
+            // },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./index.html",
+            filename: "./index.html"
+        }),
+        // new CopyWebpackPlugin([
+        //     {from: './public/assets', to: 'assets'},
+        // ],
+    //     {copyUnmodified: false}
+    // )
+    ],
+};
