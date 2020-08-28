f735f731104dbaba89f95e4312a39bf15761b786
Jason Lee Sui Yuen <jsonlsy@gmail.com>
2020-08-28 11:07:18 +1000

    Added eslint

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'http://127.0.0.1:8081';
+
+function App() {
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    return () => socket.disconnect();
+  });
+
+  return (
+    <h3>Sequence Client</h3>
+  );
+}
+
+export default App;
