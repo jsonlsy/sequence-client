43013bea60b02be2834f6c894ffcd42aec09a5f3
Jason Lee Sui Yuen <jsonlsy@gmail.com>
2020-09-05 16:39:19 +1000

    Discard dead cards

--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -4,14 +4,21 @@ import { useSelector } from 'react-redux';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 import { default as PlayingCard } from './Card';
 
 const Hand = () => {
   const cards = useSelector((state) => state.hand.cards);
   const deck = useSelector((state) => state.deck.deck);
+  const socket = useSelector((state) => state.socket);
+  const selectedCardCode = useSelector((state) => state.hand.selected);
 
   if (!cards) return null;
+
+  const discard = () => {
+    socket.emit('discard', { cardCode: selectedCardCode });
+  };
 
   return (
     <Card>
@@ -30,10 +37,23 @@ const Hand = () => {
         </Row>
       </Card.Body>
       <Card.Footer>
-        <small>
-          Cards in deck:&nbsp;
-          {deck}
-        </small>
+        <Row className="justify-content-between">
+          <Col sm="auto">
+            <small>
+              Cards in deck:&nbsp;
+              {deck}
+            </small>
+          </Col>
+          {
+            selectedCardCode && (
+              <Col sm="auto">
+                <Button size="sm" variant="secondary" onClick={discard}>
+                  Discard
+                </Button>
+              </Col>
+            )
+          }
+        </Row>
       </Card.Footer>
     </Card>
   );
